151ecdab1a5a6cd3d15bd7293a873ba2e8fe7990
dnemirich <dnemirich@gmail.com>
2025-07-13 16:41:31 +0200

    feat: add loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-8 w-8 border-4 border-orange-500 border-t-transparent"></div>
+    </div>
+
+  )
+}
