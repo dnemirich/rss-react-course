d60a7022492f3acd9c66fc994838b100378834ee
dnemirich <dnemirich@gmail.com>
2025-07-13 15:29:33 +0200

    feat: add preliminary version of an app component with requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from './components/Header.tsx';
+import { Main } from './components/Main.tsx';
+import { Component } from 'react';
+import { fetchCharacters } from './api/characters-api.ts';
+import type { Character } from './types/types.ts';
 
-function App() {
-  const [count, setCount] = useState(0)
+type AppState = {
+  searchTerm: string;
+  isLoading: boolean;
+  results: Character[];
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+type AppProps = {};
+
+export class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      searchTerm: localStorage.getItem('searchTerm') || '',
+      isLoading: false,
+      results: [],
+    };
+  }
+
+  async componentDidMount() {
+    await this.fetchAndSetCharacters(this.state.searchTerm);
+  }
+
+  fetchAndSetCharacters = async (searchTerm: string) => {
+    this.setState({ isLoading: true });
+    try {
+      const data = await fetchCharacters(searchTerm);
+      console.log(data);
+      this.setState({ results: data ?? [] });
+    } catch (error) {
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  handleChange = (value: string) => {
+    this.setState({ searchTerm: value.trim() });
+  };
+
+  handleSearch = () => {
+    localStorage.setItem('searchTerm', this.state.searchTerm);
+    this.fetchAndSetCharacters(this.state.searchTerm);
+  };
+
+  render() {
+    return (
+      <div className={'flex flex-col items-center gap-20'}>
+        <Header
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+          onSearch={this.handleSearch}
+        />
+        <Main data={this.state.results} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
-
-export default App
